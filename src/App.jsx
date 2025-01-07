6abb3a6fad792a703a88e7ba029185d95d3216a7
Denis <ponomarevd61@gmail.com>
2025-01-07 17:40:36 +0300

    edit package

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import Home from './pages/home';
 import About from './pages/about';
 import Contacts from './pages/contacts';
 import axios from 'axios';
-import LoginPage from './pages/signin';
-import RegisterPage from "./pages/req.jsx";
+import { retrieveLaunchParams } from '@telegram-apps/sdk';
+
+const { initDataRaw} = retrieveLaunchParams();
 
 const api = axios.create({
   baseURL: `http://85.208.87.10:80/api/`
@@ -15,9 +16,8 @@ const api = axios.create({
 
 async function tg_auth(userId) {
   try {
-      // Отправляем POST-запрос с userId на сервер
-      await api.post(`auth/login/${userId}`); // Правильное форматирование строки
-  
+      await api.post(`auth/login/${userId}`); 
+
   } catch (error) {
       console.log('Ошибка при авторизации:', error.message);
       return null;
@@ -25,19 +25,9 @@ async function tg_auth(userId) {
 }
 function App() {
   useEffect(() => {
-    // Убедимся, что Telegram API доступен
-    if (window.Telegram?.WebApp) {
-        window.Telegram.WebApp.ready();
 
-        const userId = window.Telegram.WebApp.initDataUnsafe?.user?.id;
-
-        if (userId) {
-            // Выполняем функцию tg_auth с userId, если он доступен
-            tg_auth(userId);
-        } else {
-            console.log('Не удалось получить ID пользователя');
-        }
-    }
+    tg_auth(userId);
+        
   }, []);
 
   return (
