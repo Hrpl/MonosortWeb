3bc23cddc916c8367fd64631ce33caa5c83411f5
sergey <makarkapar@gmail.com>
2025-03-29 18:55:01 +0300

    update modal card

--- src/pages/productCard.jsx
+++ src/pages/productCard.jsx
@@ -1,17 +1,9 @@
-import React, { useState } from 'react';
-import {
-  Box,
-  Typography,
-  IconButton,
-  Button,
-  Chip,
-  Dialog,
-  DialogContent,
-  Slide,
-} from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Typography, IconButton, Dialog, DialogContent, Slide } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme, createTheme } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
 import { forwardRef } from 'react';
 import SizeSelector from './volumeSelector';
 import "../styles/card.css";
@@ -25,15 +17,41 @@ const CoffeeCustomizer = ({ open, setDialogOpen, product }) => {
   if(product == null) return null;
   const [volume, setVolume] = useState('M');
   const [price, setPrice] = useState(250);
+	const [selectedAditives, setSelectedAdditives] = useState(1);
+  const [additivesCategories, setAdditivesCategories] = useState([]);
+	const [additives, setAdditives] = useState([]);
 
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   const handleVolumeChange = (newValue) => {
     setVolume(newValue);
-    setPrice(newValue === 'S' ? 200 : newValue === 'M' ? 250 : 300); // Update price
+    setPrice(newValue === 'S' ? 200 : newValue === 'M' ? 250 : 300);
   };
 
+	const getAdditives = () => {
+		axios.get("https://monosortcoffee.ru/api/additive/type")
+		.then((res) => {
+			setAdditivesCategories(res.data);
+		})
+		.catch((err) => {
+			console.log(err);
+		})
+	}
+	
+	useEffect(() => {
+		getAdditives();
+	}, []);
+
+	useEffect(() => {
+		axios.get(`https://monosortcoffee.ru/api/additive/many/${selectedAditives}`)
+		.then((res) => {
+			setAdditives(res.data);
+		})
+		.catch((err) => {
+			console.log(err);
+		})
+	}, [selectedAditives])
   return (
     <Dialog 
 			open={open} 
@@ -49,7 +67,7 @@ const CoffeeCustomizer = ({ open, setDialogOpen, product }) => {
           p: 0,
           bgcolor: '#222',
           color: '#fff',
-          overflow: 'hidden',
+          overflowY: 'auto',
           borderRadius: 0,
         }}
       >
@@ -80,11 +98,36 @@ const CoffeeCustomizer = ({ open, setDialogOpen, product }) => {
             {product.name}
           </Typography>
           <Typography variant="body2">настрой как любишь</Typography>
-						<div
-							className='modal__panel'
-						>
-							<SizeSelector id={product.id}/>
+					<div className="modal__additives">
+						<div className="modal__categories-list">
+							{additivesCategories.map((category) => (
+								<button 
+									key={category.id} 
+									className="modal__categories-list__item"
+									onClick={() => setSelectedAdditives(category.id)}
+								>
+									<img className="img" src={category.photo} alt="Иконка" />
+									<p className="title">{category.name}</p>
+								</button>
+								)
+							)}
 						</div>
+						<div className="modal__grid" style={{display: "grid", gridTemplateColumns: `repeat(${Math.ceil(additives.length / 2) > 3 ? Math.ceil(additives.length / 2) : 4}, 100px)`, gap: "1rem"}}>
+							{additives.map((additive) => (
+								<button 
+									key={additive.id} 
+									className="modal__grid-item"
+								>
+									<img className="img" src={additive.photo} alt="Картинка" />
+									<p className="title">{additive.name}</p>
+									<span className="price">+{additive.price} ₽</span>
+								</button>
+							))}
+						</div>
+					</div>
+					<div className='modal__panel'>
+						<SizeSelector id={product.id}/>
+					</div>
         </div>
       </DialogContent>
     </Dialog>
